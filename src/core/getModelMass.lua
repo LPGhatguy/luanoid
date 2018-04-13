cea983811a44709c40b0c984eb466bb79040a55b
Parker Stebbins <parkerstebbins@gmail.com>
2018-04-13 13:41:34 -0700

    Fix mass calculation
    Parts must now be rigidly connected to the PrimaryPart to in order to count towards the Model's mass.
    This fixes a bug where too much force is applied to the HRP when the character loses a limb.

--- src/core/getModelMass.lua
+++ src/core/getModelMass.lua
@@ -1,14 +1,20 @@
 local function getModelMass(model)
 	local mass = 0
-
-	for _, child in ipairs(model:GetChildren()) do
-		if child:IsA("BasePart") then
-			mass = mass + child:GetMass()
-		else
-			mass = mass + getModelMass(child)
+	local visited = {}
+	
+	local function sumMass(part)
+		mass = mass + part:GetMass()
+		visited[part] = true
+		
+		for _, p in ipairs(part:GetConnectedParts()) do
+			if not visited[p] then
+				sumMass(p)
+			end
 		end
 	end
-
+	
+	sumMass(assert(model.PrimaryPart))
+	
 	return mass
 end
 
